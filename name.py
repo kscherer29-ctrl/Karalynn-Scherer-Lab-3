cb40c632ba505964106938b5693f5667bb815c84
kscherer29-ctrl <kscherer29@setoncatholic.org>
2026-09-23 18:27:04 +0000

    first commit

---
+++ name.py
@@ -0,0 +1,23 @@
+# Function that adds two numbers 
+def add(x,y):
+    print(x+y)
+
+ # Function that substracts two numbers 
+def subtract(x,y):
+    print(x-y)
+
+# Function that multiplies two numbers 
+def multiply(x,y):
+    print(x*y)
+
+# Function that divides two numbers 
+def divide(x,y):
+    print(x/y)
+
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+
+add(x,y)
+subtract(x,y)
+multiply(x,y)
+divide(x,y)
