4791a53f28033f3c5225bb0166b7ed2a7216a903
kscherer29-ctrl <kscherer29@setoncatholic.org>
2026-09-24 21:36:38 +0000

    first commit

--- name.py
+++ name.py
@@ -14,10 +14,41 @@ def multiply(x,y):
 def divide(x,y):
     print(x/y)
 
-x = int(input("Enter the first number: "))
-y = int(input("Enter the second number: "))
+###########################################################
+######################Start of program#####################
 
-add(x,y)
-subtract(x,y)
-multiply(x,y)
-divide(x,y)
+while(True):
+    print("Welcome to mah awesome calc app!!!")
+    print("What would you like to do?")
+    print("Type (a)dd (s)ubtract (m)ultiply (d)ivide (e)aster egg (q)uit")
+
+    user_choice = input(": ")
+
+    if user_choice == 'q':
+        print("Shutting down")
+        break
+
+    if user_choice == 'e':
+        guess = float(input("Guess the easter egg number: "))
+        if guess == 9.75:
+            print("Off to Hogwarts!!!🦉🪄🌟🏆")
+    
+
+
+    x = float(input("Enter your first number: "))
+    y = float(input("Enter your second number: "))
+
+    if user_choice == 'a':
+        add(x,y)
+
+    elif user_choice == 's':
+        subtract(x,y)
+
+    elif user_choice == 'm':
+        multiply(x,y)
+
+    elif user_choice == 'd':
+        divide(x,y)
+
+    else:
+        print("Invalid choice")6
